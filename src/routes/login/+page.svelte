2be3ed2d2402fb787b359e1b079112494f7b6cfd
nanstey <anstey.ng@gmail.com>
2023-07-28 15:05:29 -0700

    implement google auth

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { auth, user } from "$lib/firebase";
+  import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+  async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const user = await signInWithPopup(auth, provider);
+    console.log(user);
+  }
+</script>
+
+<h1>Sign In</h1>
+
+{#if $user}
+  <h2 class="card-title">Welcome, {$user.displayName}</h2>
+  <p class="text-center text-success">You are signed in</p>
+  <button class="btn btn-danger" on:click={() => signOut(auth)}>
+    Sign out
+  </button>
+{:else}
+  <button class="btn" on:click={signInWithGoogle}>Sign in with Google</button>
+{/if}
